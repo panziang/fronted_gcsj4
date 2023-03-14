405f4fc5b08fd0f9b92c94298291cf97be32a18c
pan_ziang <2385863274@qq.com>
2023-03-14 13:37:41 +0800

    初始化

---
+++ vue3_trip/src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="app">
+    <router-view></router-view>
+    <tab-bar />
+  </div>
+</template>
+
+<script setup>
+  import tabBar from './components/tab-bar/tab-bar.vue';
+</script>
+
+<style scoped></style>
